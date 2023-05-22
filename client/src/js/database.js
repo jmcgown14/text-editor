8c63da99f3a0c80ccae479935631da481642717b
Johnny McGown <jmac3292@gmail.com>
2023-05-21 21:19:38 -0600

    trying to fix console log erros

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,8 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  const db = await openDB();
+  console.error('putDb not implemented')
+  const db = await openDB('jate', 1);
   const tx = db.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
   const request = store.put({ id: 1, value: content });
@@ -24,7 +25,8 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  const db = await openDB();
+  console.error('getDb not implemented')
+  const db = await openDB('jate', 1);
   const tx = db.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const request = store.getAll();
