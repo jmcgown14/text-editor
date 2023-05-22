c44d1a9d5b6bb429e4f41dc50348e4a920f5005d
Johnny McGown <jmac3292@gmail.com>
2023-05-21 21:04:36 -0600

    Trying to fix console log erros

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,21 +3,24 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    event.preventDefault();
-    deferredPrompt = event;
-    butInstall.style.display = 'block';
-  });
+  event.preventDefault();
+  deferredPrompt = event;
+  butInstall.style.display = 'block';
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
+  if (deferredPrompt) {
     deferredPrompt.prompt();
     const choiceResult = await deferredPrompt.userChoice;
     deferredPrompt = null;
     butInstall.style.display = 'none';
     console.log(choiceResult.outcome);
-  });
+  }
+});
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     console.log('App installed!', event);
+    window.deferredPrompt = null;
 });
